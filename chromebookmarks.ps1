8a3c73977cfd69ba56cbd2556c41a1f460f7ff8d
djust270 <57944742+djust270@users.noreply.github.com>
2019-11-20 21:30:42 -0500

    Update chromebookmarks.ps1

--- chromebookmarks.ps1
+++ chromebookmarks.ps1
@@ -6,23 +6,23 @@ $jsonRepresentation = '{
             "date_added": "13218554400308142",
             "guid": "278dff65-0d68-4b8d-9cf8-60ea3b2dbe9d",
             "id": "5",
-            "name": "ParkPlace",
+            "name": "Amazon",
             "type": "url",
-            "url": "http://parkplace.pkhotelsandresorts.com/"
+            "url": "http://amazon.com/"
          }, {
             "date_added": "13218554442843596",
             "guid": "3d87584d-a71f-4ff0-aa18-0c79efcc29c7",
             "id": "6",
-            "name": "Workday",
+            "name": "CNN",
             "type": "url",
-            "url": "http://pkotelsandresorts.com/workday"
+            "url": "http://cnn.com"
          }, {
             "date_added": "13218554452536920",
             "guid": "179fa996-6b4b-4d9a-beb7-de96fa0d848a",
             "id": "7",
-            "name": "DayForce",
+            "name": "Adobe",
             "type": "url",
-            "url": "http://pkotelsandresorts.com/time"
+            "url": "http://adobe.com"
          }, {
             "date_added": "13218554465874578",
             "guid": "c03a072c-9956-4429-9737-cc770ecd34c5",
@@ -70,7 +70,6 @@ $jsonRepresentation = '{
 $users = Get-ChildItem "C:\Users" -Exclude Public
 $users | ForEach-Object {
 New-Item -ItemType Directory -Path "C:\Users\$($_.Name)\AppData\Local\Google\chrome\User Data\default" -force} 
-
 
 $users | ForEach-Object {
 Add-Content -path "C:\Users\$($_.Name)\AppData\Local\Google\chrome\User Data\default\bookmarks" $jsonRepresentation }
