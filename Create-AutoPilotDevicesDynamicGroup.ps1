fc2d3c8ea6cc64c5d3336f0c2d476f05f37b16d1
David Just <57944742+djust270@users.noreply.github.com>
2021-03-28 16:49:31 -0400

    Add files via upload

---
+++ Create-AutoPilotDevicesDynamicGroup.ps1
@@ -0,0 +1,21 @@
+﻿Install-Module azureadpreview -AllowClobber -Force -Confirm:$false
+Import-Module AzureADPreview
+Connect-AzureAD
+
+#Create Dynamic Device Group for Autopilot
+
+$AutoPilotGroup = @{
+    DisplayName = "Autopilot Devices"
+    Description ="Dynamic Group for Autpilot Devices"
+    GroupTypes = "DynamicMembership"
+    Membershiprule = '(device.devicePhysicalIDs -any _ -contains "[ZTDId]")'
+    MailEnabled = $False
+    MailNickName = 'AutopilotDevices'
+    SecurityEnabled = $True
+    MembershipRuleProcessingState = "On"
+        }
+if ((Get-AzureADMSGroup | Where Displayname -like "Autopilot Devices") -eq $Null){
+
+New-AzureADMSGroup @AutopilotGroup
+}
+Disconnect-AzureAD
