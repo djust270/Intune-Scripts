8f5b901d62737adc69f5de67e2e4d4319127aef3
David Just <57944742+djust270@users.noreply.github.com>
2021-10-26 16:18:32 -0400

    Update DriveMap+Logonscript.ps1

--- DriveMap+Logonscript.ps1
+++ DriveMap+Logonscript.ps1
@@ -1,24 +1,28 @@
-﻿$gmapscript= '$computer = Test-Connection -ComputerName computer.domain.local -ErrorAction stop
-if ($computer -eq $true) {
-net use G: /delete
-net use G: \\computer.domain.local\share }'
+$ServerName = #Enter the FQDN of the file server
+$DriveLetter = #Enter drive letter
+$TaskName = $DriveLetter + " Drive Map"
+$UNCPath = #Enter UNC file path for fileshare
 
+$tasks = get-scheduledtask -taskpath '\' | select -ExpandProperty taskname
+if ($tasks -like $TaskName){Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false ; write-host "Task Unregistered" }
+$gmapscript= @"
+sleep 20
+`$fp01 = Test-Connection -ComputerName $ServerName -ErrorAction SilentlyContinue
+`$UNCPath = $UNCPath
+if (`$fp01) {
+net use $($DriveLetter): /delete
+net use $($DriveLetter): `$UNCPath
+}
+"@
+if ( -not (Test-Path -Path c:\automation) )
+  {
 new-item c:\automation -ItemType directory -force
-set-content c:\automation\Gdrivemap.ps1 $gmapscript
+ }
+set-content c:\automation\$($DriveLetter + 'drivemap.ps1') $gmapscript
 
-if( -Not (Get-ScheduledTask -TaskName "G Drive Map" -ErrorAction SilentlyContinue -OutVariable task) )
-    {
-        $Params = @{
-        Action = (New-ScheduledTaskAction -Execute 'powershell' -Argument '-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass c:\automation\Gdrivemap.ps1')
-        Trigger = (New-ScheduledTaskTrigger -AtLogOn)
-        Principal = (New-ScheduledTaskPrincipal -GroupId "System")
-        TaskName = 'G Drive Map'
-        Description = 'G Drive Map'
-        }
-        Register-ScheduledTask @Params
-        Start-ScheduledTask -TaskName "G Drive Map"
-    }
-    else
-    {
-        Start-ScheduledTask -TaskName "G Drive Map"
-        }
+$action = New-ScheduledTaskAction -Execute 'powershell' -Argument "-ExecutionPolicy Bypass -NoProfile -WindowStyle Minimized -File $($DriveLetter + 'drivemap.ps1')"
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+$principal = New-ScheduledTaskPrincipal -UserId (Get-CimInstance –ClassName Win32_ComputerSystem | Select-Object -expand UserName)
+$task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
+Register-ScheduledTask $TaskName -InputObject $task
+Start-ScheduledTask -TaskName $TaskName
