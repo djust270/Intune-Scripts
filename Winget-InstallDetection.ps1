679dc01901c9d31dc78309e8e311e90bcab38ed7
David Just <57944742+djust270@users.noreply.github.com>
2022-03-08 15:55:06 -0500

    Add files via upload

---
+++ Winget-InstallDetection.ps1
@@ -0,0 +1,54 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.195
+	 Created on:   	3/7/2022 2:14 PM
+	 Created by:   	Dave Just
+	 Organization: 	
+	 Filename: WingetInstallDetection.ps1   	
+	===========================================================================
+.DESCRIPTION
+	Simple Win32App detection script. Detects the presence of an uninstall key matching the displayname of the variable $SoftwareName. 
+	If a key is matched, return to Intune that the software is installed. 
+.EXAMPLE
+$SoftwareName = 'Chrome' # Search for an uninstall key with Displayname 'Chrome' for Google Chrome
+#>
+# Edit the software displayname below
+
+$SoftwareName = ''
+
+function Get-RegUninstallKey
+{
+	param (
+		[string]$DisplayName
+	)
+	$ErrorActionPreference = 'Continue'
+	#$UserSID = (New-Object -ComObject Microsoft.DiskQuota).TranslateLogonNameToSID((Get-CimInstance -Class Win32_ComputerSystem).Username)
+	$uninstallKeys = "registry::HKLM\Software\Microsoft\Windows\CurrentVersion\Uninstall", "registry::HKLM\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
+	$softwareTable = @()
+	
+	foreach ($key in $uninstallKeys)
+	{
+		$softwareTable += Get-Childitem $key | Get-ItemProperty | where displayname | Sort-Object -Property displayname
+	}
+	if ($DisplayName)
+	{
+		$softwareTable | where displayname -Like "*$DisplayName*"
+	}
+	else
+	{
+		$softwareTable | Sort-Object -Property displayname -Unique
+	}
+	
+}
+
+$UninstallKey = Get-RegUninstallKey -DisplayName $SoftwareName
+if ($UninstallKey)
+{
+	Write-Output "$($SoftwareName) is installed"
+	exit 0
+}
+else
+{
+	exit 1
+}
---
+++ Winget-InstallPackage.ps1
@@ -0,0 +1,192 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2021 v5.8.195
+	 Created on:   	3/7/2022 2:14 PM
+	 Created by:   	Dave Just
+	 Organization: 	
+	 Filename: Winget-InstallPackage.ps1   	
+	===========================================================================
+.DESCRIPTION
+	Installs any package within the WinGet public repository running as SYSTEM. Can be packaged and deployed as a Win32App in Intune
+	Use as base for any install with WinGet. Simply specify the PackageID and log variables. 
+.PARAMETER PackageID
+Specify the WinGet ID. Use WinGet Search "SoftwareName" to locate the PackageID
+    .EXAMPLE
+powershell.exe -exectuionpolicy bypass -file  Winget-InstallPackage.ps1 -PackageID Google.Chrome -Log ChromeWingetInstall.log
+	.EXAMPLE
+powershell.exe -executionpolicy bypass -file Winget-InstallPackage.ps1 -PackageID Notepad++.Notepad++ -Log NotepadPlusPlus.log
+#>
+param (
+	$PackageID,
+	$Log
+)
+
+# Re-launch as 64bit process (source: https://z-nerd.com/blog/2020/03/31-intune-win32-apps-powershell-script-installer/)
+$argsString = ""
+If ($ENV:PROCESSOR_ARCHITEW6432 -eq "AMD64")
+{
+	Try
+	{
+		foreach ($k in $MyInvocation.BoundParameters.keys)
+		{
+			switch ($MyInvocation.BoundParameters[$k].GetType().Name)
+			{
+				"SwitchParameter" { if ($MyInvocation.BoundParameters[$k].IsPresent) { $argsString += "-$k " } }
+				"String"          { $argsString += "-$k `"$($MyInvocation.BoundParameters[$k])`" " }
+				"Int32"           { $argsString += "-$k $($MyInvocation.BoundParameters[$k]) " }
+				"Boolean"         { $argsString += "-$k `$$($MyInvocation.BoundParameters[$k]) " }
+			}
+		}
+		Start-Process -FilePath "$ENV:WINDIR\SysNative\WindowsPowershell\v1.0\PowerShell.exe" -ArgumentList "-File `"$($PSScriptRoot)\Winget-InstallPackage.ps1`" $($argsString)" -Wait -NoNewWindow
+	}
+	Catch
+	{
+		Throw "Failed to start 64-bit PowerShell"
+	}
+	Exit
+}
+
+#region HelperFunctions
+function InstallWingetAsSystem # Install WinGet as logged on user
+{
+	$script = @'
+$hasPackageManager = Get-AppPackage -name "Microsoft.DesktopAppInstaller"
+
+	if(!$hasPackageManager)
+	{
+		$releases_url = "https://api.github.com/repos/microsoft/winget-cli/releases/latest"
+
+		[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+		$releases = Invoke-RestMethod -uri "$($releases_url)"
+		$latestRelease = $releases.assets | Where { $_.browser_download_url.EndsWith("msixbundle") } | Select -First 1
+	
+		Add-AppxPackage -Path $latestRelease.browser_download_url
+	}
+'@
+	if (!(test-path C:\automation)) { mkdir C:\automation }
+	$script | out-file C:\automation\script.ps1
+	$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-executionpolicy bypass -WindowStyle minimized -file C:\automation\script.ps1"
+	$trigger = New-ScheduledTaskTrigger -AtLogOn
+	$principal = New-ScheduledTaskPrincipal -UserId (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expand UserName)
+	$task = New-ScheduledTask -Action $action -Trigger $trigger -Principal $principal
+	Register-ScheduledTask RunScript -InputObject $task
+	Start-ScheduledTask -TaskName RunScript
+	Start-Sleep -Seconds 2
+	Unregister-ScheduledTask -TaskName RunScript -Confirm:$false
+	Remove-Item C:\automation\script.ps1
+}
+
+
+function Write-Log($message) #Log script messages to temp directory
+{
+	$LogMessage = ((Get-Date -Format "MM-dd-yy HH:MM:ss ") + $message)
+	Out-File -InputObject $LogMessage -FilePath "$Env:Temp\$Log" -Append -Encoding utf8
+}
+
+function WingetTempDownload # Download WinGet from blob storage if unable to install
+{
+	$WebClient = New-Object System.Net.WebClient
+	try
+	{
+		$WebClient.DownloadFile('https://djstorage2.blob.core.windows.net/scriptsupport/WinGet.zip', "$env:Temp\WinGet.zip")
+		$WebClient.Dispose()
+	}
+	Catch
+	{
+		Write-Log $error
+	}
+	
+	try
+	{
+		mkdir "$env:TEMP\WingetTemp" -Force
+		Expand-Archive "$env:TEMP\WinGet.zip" -destination "$Env:Temp\WingetTemp" -Force -ErrorAction 'Stop'
+		$global:Winget = "$Env:TEMP\WinGetTemp\Winget\AppInstallerCLI.exe"
+	}
+	Catch
+	{
+		Write-Log $Error
+	}
+		
+	$WebClient.Dispose()
+}
+
+function WingetRun {
+param (
+	$PackageID,
+	$RunType
+)
+	& $Winget $RunType --id $PackageID --source Winget --silent --accept-package-agreements --accept-source-agreements 
+}
+
+
+
+
+#endregion HelperFunctions
+#region Script
+
+$loggedOnUser = (gcim win32_computersystem).username
+# Get path for Winget executible
+$Winget = gci "$env:ProgramFiles\WindowsApps" -Recurse -File | where { $_.name -like "AppInstallerCLI.exe" -or $_.name -like "Winget.exe" } | select -ExpandProperty fullname
+# If there are multiple versions, select latest
+if ($Winget.count -gt 1) { $Winget = $Winget[-1] }
+$WingetTemp = gci $env:TEMP -Recurse -File | where name -like AppInstallerCLI.exe | select -ExpandProperty fullname
+# Try to install Winget if not already installed
+if (!($Winget))
+{
+	if ($loggedOnUser)
+	{
+		Write-Log -message "Attempting to install Winget as System under $($loggedOnUser)"
+		InstallWingetAsSystem
+		$Winget = gci "C:\Program Files\WindowsApps" -Recurse -File | where { $_.name -like "AppInstallerCLI.exe" -or $_.name -like "Winget.exe" } | select -ExpandProperty fullname
+		if ($Winget.count -gt 1) { $Winget = $Winget[-1] }
+		try
+		{
+			$Install = WingetRun -RunType install -PackageID $PackageID
+			Write-Log $Install
+		}
+		Catch
+		{
+			Write-Log $error[0]
+			exit 1
+		}
+		
+	}
+	else
+	{
+		try
+		{
+			
+			Write-Log "Winget not found, attempting to download now to $($env:TEMP)"
+			WingetTempDownload
+			try
+			{
+				$Install = WingetRun -RunType install -PackageID $PackageID
+				Write-Log $Install
+			}
+			Catch
+			{
+				Write-Log $error
+				exit 1
+			}
+			
+		}
+		Catch
+		{
+			Write-Log "Unable to initialize Winget. Exiting"
+			Write-Output $Error
+			exit 1
+		}
+	}
+	
+	
+}
+else
+{
+	Write-Log "Winget found at $($Winget)"
+	$Install = WingetRun -RunType install -PackageID $PackageID
+	Write-Log $Install
+}
+#endregion
+
+
