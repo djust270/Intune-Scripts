465f710e1228ed957f1f63cfe80dc21b35d250ca
David Just <57944742+djust270@users.noreply.github.com>
2022-08-23 10:14:46 -0400

    Update WingetUpgradeTask.ps1

--- WingetUpgradeTask.ps1
+++ WingetUpgradeTask.ps1
@@ -35,7 +35,7 @@ $LogDate = Get-Date -Format dd-MM-yy_HH-mm # go with the EU format day / month /
 $Log = "$LogName-$LogDate.log"
 $LogPath = "$env:ProgramData\Microsoft\IntuneManagementExtension\Logs"
 Import-Module WingetTools
-$WingetPath = Get-WingetPath
+$WingetPath = Get-WGPath
 
 # Add any apps you do not wish to get updated here (for instance apps that auto-update). Use the Winget ID
 $Blacklisted = @(
